d0a497e7a90fed65337e3608d0f7bd556fdc6d2a
Aleexy <lixingye1996@gmail.com>
2020-04-14 13:24:51 -0400

    fixing input/output path

--- BDM_HW3_Li.py
+++ BDM_HW3_Li.py
@@ -43,7 +43,7 @@ def main(sc):
                     how='inner')
               .sort('product', 'date')
 
-    dfFinal.saveAsTextFile(sys.argv[2])
+    return dfFinal.saveAsTextFile(sys.argv[2])
 
 if __name__=="__main__":
     sc = SparkContext()
