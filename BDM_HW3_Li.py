00f1f850f464323cf6b26569415a8a0d7a134243
Aleexy <lixingye1996@gmail.com>
2020-04-14 00:53:25 -0400

    HW3

---
+++ BDM_HW3_Li.py
@@ -0,0 +1,44 @@
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql import SQLContext
+import pyspark.sql.functions as func
+import csv
+
+sc = SparkContext()
+spark = SparkSession(sc)
+sqlContext = SQLContext(sc)
+
+def parseCSV(idx, part):
+    if idx==0:
+        next(part)
+    for p in csv.reader(part):
+        yield (p[1].lower(), p[7].lower(), int(p[0][:4]))
+
+rows = sc.textFile('../complaints_sample.csv').mapPartitionsWithIndex(parseCSV)
+df = sqlContext.createDataFrame(rows, ('product', 'company', 'date'))
+dfComplaintsYearly = df.groupby(['date', 'product']).count().sort('product')
+dfComplaintsYearly = dfComplaintsYearly.withColumnRenamed("count",
+                                                          "num_complaints")
+
+dfCompaniesCount = df.groupby(['date', 'product', 'company']).count()
+dfCompaniesYearly = dfCompaniesCount
+                    .groupby(['date', 'product'])
+                    .count()
+                    .sort('product')
+dfCompaniesYearly = dfCompaniesYearly
+                    .withColumnRenamed("count", "num_companies")
+
+dfMax = dfCompaniesCount.groupBy(['date', 'product']).max('count')
+dfTotal = dfCompaniesCount.groupBy(['date', 'product']).sum('count')
+dfRatio = dfMax.join(dfTotal, ['date', 'product'], how='inner')
+dfRatio = dfRatio.select('date', 'product', func.round(dfRatio[2]/dfRatio[3]*100)
+                 .cast('integer')
+                 .alias('percentage'))
+
+dfFinal = dfComplaintsYearly
+          .join(dfCompaniesYearly.join(dfRatio, ['date', 'product'], how='inner'),
+                ['date', 'product'],
+                how='inner')
+          .sort('product', 'date')
+
+dfFinal.write.format("csv").save('out_file')
