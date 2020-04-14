b4d8e7cf3309b85decf7e331d48948e9f94d2e8f
Aleexy <lixingye1996@gmail.com>
2020-04-14 12:36:48 -0400

    added body function

--- BDM_HW3_Li.py
+++ BDM_HW3_Li.py
@@ -5,41 +5,46 @@ import pyspark.sql.functions as func
 import sys
 import csv
 
-sc = SparkContext()
-spark = SparkSession(sc)
-sqlContext = SQLContext(sc)
 
-def parseCSV(idx, part):
-    if idx==0:
-        next(part)
-    for p in csv.reader(part):
-        yield (p[1].lower(), p[7].lower(), int(p[0][:4]))
+def main(sc):
+    spark = SparkSession(sc)
+    sqlContext = SQLContext(sc)
+    
+    def parseCSV(idx, part):
+        if idx==0:
+            next(part)
+        for p in csv.reader(part):
+            yield (p[1].lower(), p[7].lower(), int(p[0][:4]))
 
-rows = sc.textFile(str(sys.argv[1])).mapPartitionsWithIndex(parseCSV)
-df = sqlContext.createDataFrame(rows, ('product', 'company', 'date'))
-dfComplaintsYearly = df.groupby(['date', 'product']).count().sort('product')
-dfComplaintsYearly = dfComplaintsYearly.withColumnRenamed("count",
-                                                          "num_complaints")
+    rows = sc.textFile(str(sys.argv[1])).mapPartitionsWithIndex(parseCSV)
+    df = sqlContext.createDataFrame(rows, ('product', 'company', 'date'))
+    dfComplaintsYearly = df.groupby(['date', 'product']).count().sort('product')
+    dfComplaintsYearly = dfComplaintsYearly.withColumnRenamed("count",
+                                                              "num_complaints")
 
-dfCompaniesCount = df.groupby(['date', 'product', 'company']).count()
-dfCompaniesYearly = dfCompaniesCount
-                    .groupby(['date', 'product'])
-                    .count()
-                    .sort('product')
-dfCompaniesYearly = dfCompaniesYearly
-                    .withColumnRenamed("count", "num_companies")
+    dfCompaniesCount = df.groupby(['date', 'product', 'company']).count()
+    dfCompaniesYearly = dfCompaniesCount
+                        .groupby(['date', 'product'])
+                        .count()
+                        .sort('product')
+    dfCompaniesYearly = dfCompaniesYearly
+                        .withColumnRenamed("count", "num_companies")
 
-dfMax = dfCompaniesCount.groupBy(['date', 'product']).max('count')
-dfTotal = dfCompaniesCount.groupBy(['date', 'product']).sum('count')
-dfRatio = dfMax.join(dfTotal, ['date', 'product'], how='inner')
-dfRatio = dfRatio.select('date', 'product', func.round(dfRatio[2]/dfRatio[3]*100)
-                 .cast('integer')
-                 .alias('percentage'))
+    dfMax = dfCompaniesCount.groupBy(['date', 'product']).max('count')
+    dfTotal = dfCompaniesCount.groupBy(['date', 'product']).sum('count')
+    dfRatio = dfMax.join(dfTotal, ['date', 'product'], how='inner')
+    dfRatio = dfRatio.select('date', 'product', func.round(dfRatio[2]/dfRatio[3]*100)
+                     .cast('integer')
+                     .alias('percentage'))
 
-dfFinal = dfComplaintsYearly
-          .join(dfCompaniesYearly.join(dfRatio, ['date', 'product'], how='inner'),
-                ['date', 'product'],
-                how='inner')
-          .sort('product', 'date')
+    dfFinal = dfComplaintsYearly
+              .join(dfCompaniesYearly.join(dfRatio, ['date', 'product'], how='inner'),
+                    ['date', 'product'],
+                    how='inner')
+              .sort('product', 'date')
 
-dfFinal.write.format("csv").save(str(sys.argv[2]))
+    dfFinal.write.format("csv").save(str(sys.argv[2]))
+
+if __name__=="__main__":
+    sc = SparkContext()
+    main(sc)
